f5722ff81b556c23f7b8b3e58703f10229093862
tanzeelafaheem <tanzeelafaheem20@gmail.com>
2026-06-30 00:39:26 +0530

    auth route

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import authRoutes from './routes/authRoutes.js'
 
 import connectDB from "./config/db.js";
 
@@ -12,6 +13,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("FitKit Backend Running...");
